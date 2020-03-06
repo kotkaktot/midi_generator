f11fef1bbfe4e1dceca23f2a509c76f69aece081
Igor Vorobyev <ksarek@ya.ru>
2020-03-06 22:39:50 +0300

    Update midi_generator.py

--- midi_generator.py
+++ midi_generator.py
@@ -1,25 +1,27 @@
 from midiutil import MIDIFile
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from settings import token
+import os
+import time
 
 updater = Updater(token, use_context=True)
 
 
-def corona_notes_list():
+def corona_notes_list(octave):
     # reading input file with corona_virus DNA
     dna_str = ''
     for i in open('corona_dna').readlines():
         dna_str += ''.join(i.split(' ')[1:])
     dna_str = dna_str.replace('\n', '')
-    print(dna_str)
+    # print(dna_str)
 
     # result replacing with note's codes
-    octave = 4
+    octave = int(octave)
     dna_str = dna_str.replace('a', '{},'.format(33 + 12 * (octave-1)))
     dna_str = dna_str.replace('c', '{},'.format(24 + 12 * (octave-1)))
     dna_str = dna_str.replace('g', '{},'.format(31 + 12 * (octave-1)))
     dna_str = dna_str.replace('t', '{},'.format(29 + 12 * (octave-1)))
-    print(dna_str)
+    # print(dna_str)
 
     midi = []
     for note in dna_str[:-1].split(','):
@@ -28,38 +30,41 @@ def corona_notes_list():
     return midi
 
 
-def midi_generate(midi):
+def midi_generate(midi, tempo, octave):
     degrees = midi  # MIDI note number
     track = 0
     channel = 0
     time = 0    # In beats
     duration = 1    # In beats
-    tempo = 300   # In BPM
     volume = 100  # 0-127, as per the MIDI standard
+    tempo = int(tempo)
 
-    MyMIDI = MIDIFile(1)  # One track, defaults to format 1 (tempo track is created automatically)
+    MyMIDI = MIDIFile(2)  # One track, defaults to format 1 (tempo track is created automatically)
     MyMIDI.addTempo(track, time, tempo)
 
     for i, pitch in enumerate(degrees):
         MyMIDI.addNote(track, channel, pitch, time + i, duration, volume)
 
-    with open("play.mid", "wb") as output_file:
+    # print("corona_{}_{}.mid".format(tempo, octave))
+
+    with open("corona_{}_{}.mid".format(tempo, octave), "wb") as output_file:
         MyMIDI.writeFile(output_file)
 
 
-def commands(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text='/add_good слово\n'
-                                  '/add_bad слово\n'
-                                  '/del_good слово\n'
-                                  '/del_bad слово\n'
-                                  '/good_list слово\n'
-                                  '/bad_list слово\n'
-                                  '/who_add слово\n'
-                                  '/leaders\n'
-                                  '/attack\n'
-                                  '/commands\n')
+def corona_midi(update, context):
+    msg_list = update.message.text.split(' ')
+    tempo = msg_list[1]
+    octave = msg_list[2]
+    # print(tempo, octave)
+    midi_generate(corona_notes_list(octave), tempo, octave)
+    os.system('timidity corona_{}_{}.mid -Ow -o - |'
+              ' ffmpeg -i - -acodec libmp3lame -ab 64k corona_{}_{}.mp3'.format(tempo, octave, tempo, octave))
 
-updater.dispatcher.add_handler(CommandHandler('commands', commands))
+    time.sleep(5)
+    context.bot.send_audio(chat_id=update.effective_chat.id,
+                           audio=open('corona_{}_{}.mp3'.format(tempo, octave), 'rb'))
+
+
+updater.dispatcher.add_handler(CommandHandler('corona', corona_midi))
 updater.start_polling()
 updater.idle()
